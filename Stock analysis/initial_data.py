6ca3001cc0170ad3a9d541208f6d877f7c285e82
Tien <tien.le.290803@gmail.com>
2023-03-03 13:17:08 +0700

    Final result

---
+++ Stock analysis/initial_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import cufflinks as cf
+import investpy
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+
+label = ['AAA','AAM','AAT','ABR','ABS','ABT','ACB','ACC','ACL','ADG','ADS','AGG','AGM','AGR','AMD','ANV','APC','APC','APG','APH','ASG','ASG'
+,'ASM','ASP','AST','BAF','BBC','BCE','BCG','BCM','BFC','BHN','BIC','BID','BKG','BMC','BMI','BMP','BRC','BSI','BTP','BTT','BVH','BWE','C32'
+,'C47','CAV','CCI','CCL','CDC','CHP','CIG','CII','CKG','CLC','CLL','CLW','CMG','CMV','CMX','CNG','COM','CRC','CRE','CSM','CSV','CTD','CTF'
+,'CTG','CTI','CTR','CVT','D2D','DAG','DAH','DAT','DBC','DBT','DC4','DCL','DCM','DGC','DGW','DHA','DHC','DHG','DHM','DIG','DLG','DMC','DPG'
+,'DQC','DRC','DRH','DRL','DSN','DTA','DTL','DTT','DVP','DXG','DXS','DXV','EIB','ELC','EMC','EVE','EVF','EVG','FMC','FMC','FPT','FRT','FTS'
+,'GAB','GAS','GDT','GEG','GEX','GIL','GMD','GSP','GTA','GVR','HAG','HAH','HAI','HAP','HAR','HAS','HAX','HBC','HCD','HCM','HDB','HDC','HDG'
+,'HHP','HHS','HID','HII','HMC','HNG','HOT','HPG','HPX','HQC','HRC','HSG','HSL','HT1','HTI','HTL','HTN','HTV','HU1','HU3','HUB','HVB','HVH'
+,'HVX','IBC','ICT','IDI','IJC','ILB','IMP','ITA','ITC','ITD','JVC','KBC','KDC','KHG','KHP','KMR','KOS','KPF','KSB','L10','LAF','LBM','LCG'
+,'LDG','LGL','LHG','LIX','LM8','LPB','LSS','MBB','MCG','MCP','MDG','MHC','MIG','MSB','MSH','MSN','MWG','NAF','NAV','NAV','NCT','NHA','NHH'
+,'NHT','NKG','NLG','NNC','NSC','NT2','NTL','NVL','NVT','OCB','OGC','OPC','ORS','PAC','PAN','PC1','PDN','PDR','PET','PGC','PGD','PGI','PGV'
+,'PHC','PHR','PIT','PJT','PLP','PLX','PMG','PNC','PNJ','POM','POW','PPC','PSH','PTB','PTC','PTL','PVD','PVT','QBS','QCG','RAL','RDP','REE'
+,'S4A','SAB','SAM','SAV','SBA','SBT','SBV','SC5','SCD','SCR','SCS','SFC','SFG','SFI','SGN','SGR','SGT','SHA','SHB','SHI','SHP','SII','SJD'
+,'SJF','SJS','SKG','SMA','SMB','SMC','SPM','SRC','SRF','SSB','SSC','SSI','ST8','STG','STK','SVC','SVD','SVI','SVT','SZC','SZL','TBC','TCB'
+,'TCD','TCH','TCL','TCM','TCO','TCR','TCT','TDC','TDG','TDH','TDM','TDP','TDW','TEG','TGG','THG','THI','TIP','TIX','TLD','TLG','TLH','TMP'
+,'TMS','TMT','TN1','TNA','TNC','TNH','TNI','TNT','TPB','TPC','TRA','TRC','TSC','TTA','TTB','VJC','VMD','VND','VNE','VNG','VNL','VNM','VNS'
+,'VOS','VPB','VPD','VPH','VPI','VPS','VRC','VRE','VSC','VSH','VSI','VTB','VTO','YBM','YEG']
+for name in label:
+    print(f"{name} = pd.read_excel('stock data\{name} Historical Data.xlsx')")
